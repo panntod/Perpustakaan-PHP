adc012720f1fa8174abb12972825896116d150b1
panntod <pandhu.munjalindra@gmail.com>
2024-03-30 09:36:37 +0700

    refactor: Change icon logo and gap navbar

---
+++ frontend/footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-light text-center py-3">
+    <div class="container">
+        <p>&copy; 2023 <a href="https://pandhu-munjalindra.xyz" class="text-decoration-none fw-semibold text-white">Pandhu Arya</a></p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
